37fe708b6dd4103c0ede85f212b8a0b00f7bf784
jklins-ects <73347118+jklins-ects@users.noreply.github.com>
2025-01-15 22:16:02 -0500

    Made it so it only returns data when a term is searched.

--- controllers/views/homeController.js
+++ controllers/views/homeController.js
@@ -1,19 +1,23 @@
 const axios = require("axios");
 const settings = require("../../config/settings");
 async function renderHomePage(req, res) {
-    const response = await axios.get(
-        `${settings.ROOT}:${settings.PORT}/api/games`,
-        { params: req.query }
-    );
-
-    const data = response.data;
-    for (game of data) {
-        const cover = await axios.get(
-            `${settings.ROOT}:${settings.PORT}/api/covers`,
-            { params: { gameid: game.game_id } }
+    let data = [];
+    if (req.query.like) {
+        const response = await axios.get(
+            `${settings.ROOT}:${settings.PORT}/api/games`,
+            { params: req.query }
         );
-        game.cover = cover.data[0] ? cover.data[0].url : "";
+
+        data = response.data;
+        for (game of data) {
+            const cover = await axios.get(
+                `${settings.ROOT}:${settings.PORT}/api/covers`,
+                { params: { gameid: game.game_id } }
+            );
+            game.cover = cover.data[0] ? cover.data[0].url : "";
+        }
     }
+
     res.render("home", {
         title: "Games!",
         search: req.query.like ? req.query.like : "",
