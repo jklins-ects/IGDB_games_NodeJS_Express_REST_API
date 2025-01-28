d2c389fbca285253062f0c0fa354f44e46c91001
jklins-ects <73347118+jklins-ects@users.noreply.github.com>
2025-01-28 09:59:33 -0500

    adding cover routes

--- routes/coverRoutes.js
+++ routes/coverRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const coverController = require("../controllers/api/coverController");
+const coverController = require("../controllers/coverController");
 
 /**
  * @swagger
