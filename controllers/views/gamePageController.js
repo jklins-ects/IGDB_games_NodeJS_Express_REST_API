3dcc325993fe1974b6a0a85a8d85a1776902a004
jklins-ects <73347118+jklins-ects@users.noreply.github.com>
2025-02-04 14:19:16 -0500

    started adding requests for game data

--- controllers/views/gamePageController.js
+++ controllers/views/gamePageController.js
@@ -1,7 +1,18 @@
 const axios = require("axios");
 const settings = require("../../config/settings");
 async function renderGamePage(req, res) {
-    let data = { game_id: req.params.id };
+    const response = await axios.get(
+        `${settings.ROOT}:${settings.PORT}/api/games/${req.params.id}`
+    );
+    data = response.data;
+    const cover = await axios.get(
+        `${settings.ROOT}:${settings.PORT}/api/covers`,
+        {
+            params: { gameid: req.params.id },
+        }
+    );
+    data.cover = cover.data[0] ? cover.data[0].url : "";
+
     res.render("game", {
         title: "Games!",
         gameData: data,
